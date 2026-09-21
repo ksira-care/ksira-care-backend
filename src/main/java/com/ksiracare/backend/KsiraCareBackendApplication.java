fd3021b74d421200353a1c96266588132bc63c15
SIDRNH <dshanmukha10@gmail.com>
2026-09-21 18:55:45 +0530

    chore: initialize Spring Boot backend

---
+++ src/main/java/com/ksiracare/backend/KsiraCareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ksiracare.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsiraCareBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KsiraCareBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ksiracare/backend/KsiraCareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksiracare.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsiraCareBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
